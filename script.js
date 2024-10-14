dfd3b1a5634a41ef5f945a2b933b3b45d1891577
realtudor7 <tanasealexandru871@gmail.com>
2024-10-14 20:53:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function clock() {
+    location.href = "index1.html"
+}
+
+function notes() {
+    location.href = "index2.html"
+}
+
+function music() {
+    location.href = "index3.html"
+}
---
+++ script3.js
@@ -0,0 +1,23 @@
+function music1() {
+    var audio = new Audio('camion.mp3');
+    audio.play();
+    
+}
+
+function music2() {
+    var audio = new Audio('diamante.mp3');
+    audio.play();
+
+}
+
+function music3() {
+    var audio = new Audio('printesa.mp3');
+    audio.play();
+
+}
+
+function music4() {
+    var audio = new Audio('zana.mp3');
+    audio.play();
+     
+}
